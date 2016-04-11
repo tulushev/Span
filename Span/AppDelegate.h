2c944634e8457a229b57604b80e1fca68628126c
Eugene Tulushev <eugene@tulushev.com>
2016-04-11 13:48:04 +0300

    Initial Commit

---
+++ Span/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Span
+//
+//  Created by Eugene Tulushev on 11/04/16.
+//  Copyright © 2016 Eugene Tulushev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Span/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Span
+//
+//  Created by Eugene Tulushev on 11/04/16.
+//  Copyright © 2016 Eugene Tulushev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
